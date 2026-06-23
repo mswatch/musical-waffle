36309dd873701b7244faeb70fbabc710af5c45c9
mswatches <mahashaktiwatches@gmail.com>
2026-06-23 17:24:36 +0530

    7th commit

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import CartDrawer from "@/components/CartDrawer";
+import Header from "./Header";
+import Footer from "./Footer";
+import CartDrawer from "./CartDrawer";
 import { Toaster } from "sonner";
 
 export default function Layout() {
